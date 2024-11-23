a437ba616bfc9f49358b971bef84486a561745f9
방준혁 <qkd3001@naver.com>
2024-11-24 04:37:16 +0900

    db연결 확인

--- src/index.js
+++ src/index.js
@@ -1,14 +1,52 @@
+// 1. 표준 라이브러리
 const express = require('express');
+
+// 2. 외부 라이브러리
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+// 3. 내부 모듈
+dotenv.config();
+const db = require('../config/database.js');
+
+// 라우터 선언 
+
+
+// Express 앱 설정
+const app = express();
+app.set('port', process.env.PORT || 8000);
+
+if (process.env.NODE_ENV === 'production') {
+  app.enable('trust proxy');
+  app.use(morgan('combined'));
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+      crossOriginEmbedderPolicy: false,
+      crossOriginResourcePolicy: false,
+    })
+  );
+} else {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+//라우터
+
+
+// 404 에러 처리 미들웨어
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
     error.status = 404;
     next(error);
   });
   
-  app.listen(app.get('port'), () => {
-    console.log(`Server is running on http://localhost:${app.get('port')}/`);
-  });
+// 서버 시작
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
